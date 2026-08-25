b0974498429e1a7aa6c63e6a9a5051575a2e5b7a
Kimi <45228918+ds-kimi@users.noreply.github.com>
2026-08-25 23:14:11 +0200

    add generator-version option

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,12 @@
-PROJECT_GENERATOR_VERSION = 2
+-- Allow CI to override via --generator-version=3 for the x86-64-support-sourcesdk
+-- branch of garrysmod_common; the PROJECT_GENERATOR_VERSION env var still works too.
+newoption({
+	trigger = "generator-version",
+	description = "Sets the garrysmod_common project generator version (2 = x86 only, 3 = adds x86-64)",
+	value = "2"
+})
+
+PROJECT_GENERATOR_VERSION = tonumber(_OPTIONS["generator-version"]) or 2
 
 newoption({
 	trigger = "gmcommon",
